b1f4b7aaa88e1d423e7174c04bd6ebcc36545a5c
Daniel <daniilpancenko2004@gmail.com>
2025-12-07 14:37:27 +0200

    Final version with 5 working tests

--- tests/academybugs.spec.js
+++ tests/academybugs.spec.js
@@ -51,42 +51,61 @@ test.describe('Фінальний набір тестів для AcademyBugs.com
   await expect(priceLocator).not.toHaveText(initialPrice);
 });
 
-// ТЕСТ #4: ПЕРЕВІРКА КОРЕКТНОСТІ ВІДОБРАЖЕННЯ КНОПКИ "RETURN TO STORE" У КОШИКУ
-test('Перевірка коректності відображення кнопки “Return to Store” у кошику', async ({ page }) => {
-  // Крок 1: Відкрити сторінку товару
-  await page.goto('https://academybugs.com/store/dnk-yellow-shoes/');
+  // ТЕСТ #4: ПЕРЕВІРКА КОРЕКТНОСТІ ВІДОБРАЖЕННЯ КНОПКИ "RETURN TO STORE" У КОШИКУ
+  test('Перевірка коректності відображення кнопки “Return to Store” у кошику', async ({ page }) => {
+    // Крок 1: Відкрити сторінку товару
+    await page.goto('https://academybugs.com/store/dnk-yellow-shoes/');
 
-  const cartWidgetButton = page.locator('.ec_cart_widget_button');
+    const cartWidgetButton = page.locator('.ec_cart_widget_button');
 
-  // Крок 2: Наводимо курсор на контейнер кошика
-  await cartWidgetButton.hover();
+    // Крок 2: Наводимо курсор на контейнер кошика
+    await cartWidgetButton.hover();
 
-  // Крок 3: CHECKOUT повинен стати видимим, після чого клікаємо на нього
-  const checkoutButton = page.locator('.ec_cart_widget_minicart_checkout_button');
-  await checkoutButton.waitFor({ state: 'visible', timeout: 5000 });
-  await checkoutButton.click();
+    // Крок 3: CHECKOUT повинен стати видимим, після чого клікаємо на нього
+    const checkoutButton = page.locator('.ec_cart_widget_minicart_checkout_button');
+    await checkoutButton.waitFor({ state: 'visible', timeout: 5000 });
+    await checkoutButton.click();
 
-  // Крок 4: Видаляємо всі товари по одному
-  let deleteButtons = page.locator('.ec_cartitem_delete');
-  while (await deleteButtons.count() > 0) {
-    await deleteButtons.first().click();
-    await deleteButtons.first().waitFor({ state: 'detached' });
-    deleteButtons = page.locator('.ec_cartitem_delete');
-  }
+    // Крок 4: Видаляємо всі товари по одному
+    let deleteButtons = page.locator('.ec_cartitem_delete');
+    while (await deleteButtons.count() > 0) {
+      await deleteButtons.first().click();
+      await deleteButtons.first().waitFor({ state: 'detached' });
+      deleteButtons = page.locator('.ec_cartitem_delete');
+    }
 
-  // Крок 5: Знаходимо кнопку
-  const returnToStoreBtn = page.locator('a.ec_cart_empty_button.academy-bug');
+    // Крок 5: Знаходимо кнопку
+    const returnToStoreBtn = page.locator('a.ec_cart_empty_button.academy-bug');
 
-  // Крок 6: Чекаємо появи кнопки
-  await returnToStoreBtn.waitFor({ state: 'visible', timeout: 15000 });
+    // Крок 6: Чекаємо появи кнопки
+    await returnToStoreBtn.waitFor({ state: 'visible', timeout: 15000 });
 
-  // Крок 7: Очищаємо текст кнопки від зайвих пробілів перетворивши декілька послідовних пробілів в один та порівнюємо з очікуваним значенням
-  const buttonText = (await returnToStoreBtn.textContent()).replace(/\s+/g, ' ').trim();
-  expect(buttonText).toBe('RETURN TO STORE');
+    // Крок 7: Очищаємо текст кнопки від зайвих пробілів перетворивши декілька послідовних пробілів в один та порівнюємо з очікуваним значенням
+    const buttonText = (await returnToStoreBtn.textContent()).replace(/\s+/g, ' ').trim();
+    expect(buttonText).toBe('RETURN TO STORE');
 
-  // Крок 8: Перевіряємо, що кнопка видима
-  await expect(returnToStoreBtn).toBeVisible();
-},{ timeout: 60000 });
+    // Крок 8: Перевіряємо, що кнопка видима
+    await expect(returnToStoreBtn).toBeVisible();
+  },{ timeout: 60000 });
 
 
+  // ТЕСТ #5: ПЕРЕВІРКА НЕДОСТУПНОСТІ КНОПОК ПРИ ЗМІНІ ВАЛЮТИ
+  test('Перевірка недоступності кнопок при зміні валюти', async ({ page }) => {
+    // Крок 1: Відкриваємо сторінку товару
+    await page.goto('https://academybugs.com/store/dnk-yellow-shoes/');
+
+    // Крок 2: Знаходимо кнопку ADD TO CART
+    const addToCartBtn = page.locator('input[type="submit"][value="ADD TO CART"]');
+
+   // Крок 3: Знаходимо кнопку + для збільшення кількості товару
+    const countBtn = page.locator('input[type="button"][value="+"]');
+
+    // Крок 4: Вибираємо валюту (наприклад EUR)
+    const currencySelect = page.locator('#ec_currency_conversion');
+    await currencySelect.selectOption('EUR');
+
+    // Крок 5: Перевіряємо, що кнопки **недоступні** протягом 5 секунд
+    await expect(addToCartBtn).toBeDisabled({ timeout: 5000 });
+    await expect(countBtn).toBeDisabled({ timeout: 5000 });
+  });
 });
